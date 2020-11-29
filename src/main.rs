a35aed09fdce1be6eb326d16ab230c5bf9962241
Fei Whang <feiwhang@Feis-MacBook-Pro.local>
2020-11-29 22:39:03 +0700

    randomize pivot

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+use rand::Rng;
+
 fn main() {
     let test_1: Vec<usize> = vec![9,18,1,6,7,8,4,12,10,6,1];
-    // println!("{:?}", _recursive_quick_sort(test_1));
+    println!("{:?}", _recursive_quick_sort(test_1));
 }
 
 fn _recursive_quick_sort(xs: Vec<usize>) -> Vec<usize> {
     if xs.len() <= 1 {
         xs
     } else {
-        let pivot: &usize = xs.last().unwrap();
+        let mut rng = rand::thread_rng();
+        let random_i: usize = rng.gen_range(0, xs.len());
+        let pivot: &usize = xs.get(random_i).unwrap();
         let less_than: Vec<usize> = xs.iter().filter(|&x| x < pivot).map(|x| *x).collect();
         let equal: Vec<usize> = xs.iter().filter(|&x| x == pivot).map(|x| *x).collect();
         let more_than: Vec<usize> = xs.iter().filter(|&x| x > pivot).map(|x| *x).collect();
@@ -20,6 +24,6 @@ fn _recursive_quick_sort(xs: Vec<usize>) -> Vec<usize> {
 }
 
 
-fn quicker_sort() {
-
-}
+// fn quicker_sort() {
+//
+// }
