77dfff8ebef31cff070b082812c787656f0b4d62
Fei Whang <feiwhang@Feis-MacBook-Pro.local>
2020-11-30 02:20:02 +0700

    change quicker_sort to 3-way

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,91 @@
+// SAMPLE INPUT FROM: https://github.com/ashwinravishankar/Sorting-Techniques
 use rand::Rng;
 use rand::rngs::ThreadRng;
-use rayon::join;
+use std::fs::File;
+use std::io::prelude::Read;
 
 fn main() {
-    // let test_1: Vec<u64> = vec![9,18,1,6,7,8,4,12,10,6,1];
-    // println!("{:?}", normal_quick_sort(test_1));
 
-    let mut test_2: Vec<u64> = vec![8,4,19,1,14,1,3,3];
-    quicker_sort(&mut test_2);
-    println!("{:?}", test_2);
+    let mut f = File::open("resources/input_100000.txt").unwrap();
+    let mut whole_file = String::new();
+    let _ = f.read_to_string(&mut whole_file);
+
+    let mut test_input: Vec<i64> = whole_file
+        .split(" ")
+        .map(|s| s.parse::<i64>().unwrap()).collect();
+
+    quicker_sort(&mut test_input);
+
+    let sorted = test_input.clone();
+    println!("{:?}", sorted);
+
+    test_input.sort_unstable();
+    assert_eq!(sorted, test_input);
+
+}
+// 3-way Quick-Sort: http://www.cs.princeton.edu/~rs/talks/QuicksortIsOptimal.pdf
+fn quicker_sort(xs: &mut Vec<i64>) {
+    fn sort_helper(xs: &mut Vec<i64>, left: i64, right: i64) {
+        if right <= left { return }
+
+        let mut i: i64 = left - 1;
+        let mut j: i64 = right.clone();
+        let mut p: i64 = left - 1;
+        let mut q: i64 = right.clone();
+        let v: i64 = *xs.get(right as usize).unwrap();
+
+        loop {
+            i += 1;
+            while *xs.get(i as usize).unwrap() < v { i += 1; }
+
+            j -= 1;
+            while v < *xs.get(j as usize).unwrap() {
+                if j == left { break }
+                j -= 1;
+            }
+
+            if i >= j { break }
+
+            xs.swap(i as usize, j as usize);
+
+            if *xs.get(i as usize).unwrap() == v {
+                p += 1;
+                xs.swap(p as usize, i as usize);
+            }
+
+            if v == *xs.get(j as usize).unwrap() {
+                q -= 1;
+                xs.swap(j as usize, q as usize);
+            }
+        }
+
+        xs.swap(i as usize, right as usize);
+        j = i - 1;
+        i += 1;
+
+        for z in left..p-1{
+            xs.swap(z as usize, j as usize);
+            j -= 1;
+        }
+
+        for z in (right-1)..q-1 {
+            xs.swap(i as usize, z as usize);
+            i += 1;
+        }
+
+        sort_helper(xs, left, j);
+        sort_helper(xs, i, right);
+
+    }
+
+    let length = xs.len();
+    if length > 1 {
+        sort_helper(xs, 0, (length - 1) as i64);
+    }
 }
 
-fn normal_quick_sort(xs: Vec<u64>) -> Vec<u64> {
+
+fn _normal_quick_sort(xs: Vec<u64>) -> Vec<u64> {
     if xs.len() <= 1 { xs }
     else {
         let mut rng: ThreadRng = rand::thread_rng();
@@ -22,53 +96,9 @@ fn normal_quick_sort(xs: Vec<u64>) -> Vec<u64> {
         let equal: Vec<u64> = xs.iter().filter(|&x| x == &pivot).map(|x| *x).collect();
         let more_than: Vec<u64> = xs.iter().filter(|&x| x > &pivot).map(|x| *x).collect();
 
-        let recurse_less_than: Vec<u64> = normal_quick_sort(less_than);
-        let recurse_more_than: Vec<u64> = normal_quick_sort(more_than);
+        let recurse_less_than: Vec<u64> = _normal_quick_sort(less_than);
+        let recurse_more_than: Vec<u64> = _normal_quick_sort(more_than);
 
         [recurse_less_than, equal, recurse_more_than].concat()
     }
-}
-
-// use swap instead
-fn quicker_sort(xs: &mut [u64]) {
-    let length: usize = xs.len();
-
-    if length == 2 {
-        if xs.first().unwrap() > xs.last().unwrap() {
-            xs.swap(0, 1);
-        }
-    }
-
-    if length > 2 {
-        // randomize pivot
-        let mut rng: ThreadRng = rand::thread_rng();
-        let mut pivot: usize = rng.gen_range(1, length);
-        let pivot_value: u64 = *xs.get(pivot).unwrap();
-
-        // move pivot to the front
-        xs.swap(0, pivot);
-        pivot = 0;
-
-        for i in 1..length {
-            // if xs[i] less than pivot value
-            // swap xs[i] with xs[pivot+1]
-            if xs.get(i).unwrap() < &pivot_value {
-                pivot += 1;
-                xs.swap(i, pivot)
-            }
-        }
-
-        // swap pivot value back to place
-        xs.swap(0, pivot);
-        // after swapping done
-        // every n less than or eq to pivot value
-        // will be on the left of pivot
-
-        let (less_than_or_eq, more_than_or_eq) = xs.split_at_mut(pivot);
-
-        join(|| quicker_sort(less_than_or_eq),
-            || quicker_sort(more_than_or_eq),);
-
-    }
-
 }
